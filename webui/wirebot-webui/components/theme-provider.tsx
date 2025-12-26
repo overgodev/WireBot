2422ffc34854d066324a921a446ce7f94b827253
overgo <sorawitpoom2546@gmail.com>
2025-12-26 18:30:45 +0700

    Update theme-provider.tsx

--- webui/wirebot-webui/components/theme-provider.tsx
+++ webui/wirebot-webui/components/theme-provider.tsx
@@ -7,9 +7,10 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="dark"
+      forcedTheme="dark"        // <- hard lock to dark
       enableSystem={false}
       disableTransitionOnChange
+      storageKey="wirebot-theme" // <- new key, ignores old `theme`
     >
       {children}
     </NextThemesProvider>
